5be5756be20903922eb83cc360b7bd44c90dc4f1
Tejaswi Kumar <97628619+Tejaswi416@users.noreply.github.com>
2022-07-20 14:39:56 +0530

    a=int(input()) s=list(map(int,input().split())) g=0 for i in s:     while(i):         d=i%10         g+=d         i//=10 print(g)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,18 +1,9 @@
-def add(n):
-    su=0
-    if n==0:
-        return 0
-    elif  n>0 and n<10:
-        su=su+n
-    else:
-        while(n!=0):
-            r=n%10
-            su=su+r
-            n=n//10
-    return su
-n=int(input())
-a=list(map(int,input().split()))[:n]
-su=0
-for i in range(len(a)):
-    su=su+add(a[i])
-print(su)
+a=int(input())
+s=list(map(int,input().split()))
+g=0
+for i in s:
+    while(i):
+        d=i%10
+        g+=d
+        i//=10
+print(g)
